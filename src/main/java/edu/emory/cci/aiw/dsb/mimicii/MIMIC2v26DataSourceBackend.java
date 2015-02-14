dbccd4a0c5045b7126c9eda9f3b7ecb758dd49a7
Andrew Post <arpost@emory.edu>
2015-02-14 08:34:49 -0500

    Fix mappings API.

--- src/main/java/edu/emory/cci/aiw/dsb/mimicii/MIMIC2v26DataSourceBackend.java
+++ src/main/java/edu/emory/cci/aiw/dsb/mimicii/MIMIC2v26DataSourceBackend.java
@@ -9,11 +9,12 @@ import org.protempa.backend.dsb.relationaldb.JDBCDateTimeTimestampDateValueForma
 import org.protempa.backend.dsb.relationaldb.JDBCDateTimeTimestampPositionParser;
 import org.protempa.backend.dsb.relationaldb.JDBCPositionFormat;
 import org.protempa.backend.dsb.relationaldb.JoinSpec;
-import org.protempa.backend.dsb.relationaldb.PropIdToSQLCodeMapper;
 import org.protempa.backend.dsb.relationaldb.PropertySpec;
 import org.protempa.backend.dsb.relationaldb.ReferenceSpec;
 import org.protempa.backend.dsb.relationaldb.RelationalDbDataSourceBackend;
 import org.protempa.backend.dsb.relationaldb.StagingSpec;
+import org.protempa.backend.dsb.relationaldb.mappings.Mappings;
+import org.protempa.backend.dsb.relationaldb.mappings.ResourceMappingsFactory;
 import org.protempa.proposition.value.AbsoluteTimeGranularity;
 import org.protempa.proposition.value.AbsoluteTimeGranularityFactory;
 import org.protempa.proposition.value.AbsoluteTimeUnit;
@@ -35,14 +36,12 @@ public class MIMIC2v26DataSourceBackend extends RelationalDbDataSourceBackend {
             new AbsoluteTimeUnitFactory();
     private static AbsoluteTimeGranularityFactory absTimeGranularityFactory =
             new AbsoluteTimeGranularityFactory();
-    private final PropIdToSQLCodeMapper mapper;
-
     public MIMIC2v26DataSourceBackend() {
-        this.mapper = new PropIdToSQLCodeMapper("/etc/mimic2v26/", getClass());
         setSchemaName("mimic2v26");
         setDefaultKeyIdTable("d_patients");
         setDefaultKeyIdColumn("subject_id");
         setDefaultKeyIdJoinKey("subject_id");
+        setMappingsFactory(new ResourceMappingsFactory("/etc/mimic2v26/", getClass()));
     }
 
     @Override
@@ -74,11 +73,11 @@ public class MIMIC2v26DataSourceBackend extends RelationalDbDataSourceBackend {
                 null, 
                 new PropertySpec[]{
                     new PropertySpec("patientId", null, new ColumnSpec(schemaName, "demographic_detail", "hadm_id"), ValueType.NOMINALVALUE), 
-                    new PropertySpec("gender", null, new ColumnSpec(schemaName, "demographic_detail", new JoinSpec("subject_id", "subject_id", new ColumnSpec(schemaName, "d_patients", "sex", Operator.EQUAL_TO, this.mapper.propertyNameOrPropIdToSqlCodeArray("gender_09102013.txt"), true))), ValueType.NOMINALVALUE), 
-                    new PropertySpec("race", null, new ColumnSpec(schemaName, "demographic_detail", "ethnicity_itemid", Operator.EQUAL_TO, this.mapper.propertyNameOrPropIdToSqlCodeArray("race_09102013.txt"), true), ValueType.NOMINALVALUE), 
-                    new PropertySpec("ethnicity", null, new ColumnSpec(schemaName, "demographic_detail", "ethnicity_itemid", Operator.EQUAL_TO, this.mapper.propertyNameOrPropIdToSqlCodeArray("ethnicity_09102013.txt"), true), ValueType.NOMINALVALUE),
+                    new PropertySpec("gender", null, new ColumnSpec(schemaName, "demographic_detail", new JoinSpec("subject_id", "subject_id", new ColumnSpec(schemaName, "d_patients", "sex", Operator.EQUAL_TO, getMappingsFactory().getInstance("gender_09102013.txt"), true))), ValueType.NOMINALVALUE), 
+                    new PropertySpec("race", null, new ColumnSpec(schemaName, "demographic_detail", "ethnicity_itemid", Operator.EQUAL_TO, getMappingsFactory().getInstance("race_09102013.txt"), true), ValueType.NOMINALVALUE), 
+                    new PropertySpec("ethnicity", null, new ColumnSpec(schemaName, "demographic_detail", "ethnicity_itemid", Operator.EQUAL_TO, getMappingsFactory().getInstance("ethnicity_09102013.txt"), true), ValueType.NOMINALVALUE),
                     new PropertySpec("dateOfBirth", null, new ColumnSpec(schemaName, "demographic_detail", new JoinSpec("subject_id", "subject_id", new ColumnSpec(schemaName, "d_patients", "dob"))), ValueType.DATEVALUE, new JDBCDateTimeTimestampDateValueFormat()),
-                    new PropertySpec("maritalStatus", null, new ColumnSpec(schemaName, keyIdTable, new JoinSpec("subject_id", "subject_id", new ColumnSpec(schemaName, "demographic_detail", "marital_status_itemid", Operator.EQUAL_TO, this.mapper.propertyNameOrPropIdToSqlCodeArray("marital_status_09102013.txt"), true))), ValueType.NOMINALVALUE)
+                    new PropertySpec("maritalStatus", null, new ColumnSpec(schemaName, keyIdTable, new JoinSpec("subject_id", "subject_id", new ColumnSpec(schemaName, "demographic_detail", "marital_status_itemid", Operator.EQUAL_TO, getMappingsFactory().getInstance("marital_status_09102013.txt"), true))), ValueType.NOMINALVALUE)
                 }, 
                 new ReferenceSpec[]{
                     new ReferenceSpec("encounters", "Encounters", new ColumnSpec[]{new ColumnSpec(schemaName, "demographic_detail", "hadm_id")}, ReferenceSpec.Type.MANY), 
@@ -91,6 +90,7 @@ public class MIMIC2v26DataSourceBackend extends RelationalDbDataSourceBackend {
     @Override
     protected EntitySpec[] eventSpecs(String keyIdSchema, String keyIdTable, String keyIdColumn, String keyIdJoinKey) throws IOException {
         String schemaName = getSchemaName();
+        Mappings icd9DxMappings = getMappingsFactory().getInstance("icd9_diagnosis_09102013.txt");
         EntitySpec[] eventSpecs = {
             new EntitySpec("Encounters", 
                 null,
@@ -102,7 +102,7 @@ public class MIMIC2v26DataSourceBackend extends RelationalDbDataSourceBackend {
                 new ColumnSpec(schemaName, "admissions", "disch_dt"), 
                 new PropertySpec[]{
                     new PropertySpec("encounterId", null, new ColumnSpec(schemaName, "admissions", "hadm_id"), ValueType.NOMINALVALUE), 
-                    new PropertySpec("type", null, new ColumnSpec(schemaName, "admissions", new JoinSpec("hadm_id", "hadm_id", new ColumnSpec(schemaName, "demographic_detail", "admission_type_itemid", Operator.EQUAL_TO, this.mapper.propertyNameOrPropIdToSqlCodeArray("type_encounter_09102013.txt"), true))), ValueType.NOMINALVALUE), 
+                    new PropertySpec("type", null, new ColumnSpec(schemaName, "admissions", new JoinSpec("hadm_id", "hadm_id", new ColumnSpec(schemaName, "demographic_detail", "admission_type_itemid", Operator.EQUAL_TO, getMappingsFactory().getInstance("type_encounter_09102013.txt"), true))), ValueType.NOMINALVALUE), 
                 }, 
                 new ReferenceSpec[]{
                     new ReferenceSpec("patient", "Patients", new ColumnSpec[]{new ColumnSpec(schemaName, "admissions", "subject_id")}, ReferenceSpec.Type.ONE), 
@@ -112,7 +112,7 @@ public class MIMIC2v26DataSourceBackend extends RelationalDbDataSourceBackend {
                 null, null, null, null, null, AbsoluteTimeGranularity.DAY, jdbcTimestampPositionParser, null),
             new EntitySpec("Diagnosis Codes", 
                 null, 
-                this.mapper.readCodes("icd9_diagnosis_09102013.txt", 0), 
+                icd9DxMappings.readTargets(), 
                 true, 
                 new ColumnSpec(keyIdSchema, keyIdTable, keyIdColumn, new JoinSpec("subject_id", "subject_id", new ColumnSpec(schemaName, "demographic_detail", new JoinSpec("hadm_id", "hadm_id", new ColumnSpec(schemaName, "admissions", new JoinSpec("hadm_id", "hadm_id", new ColumnSpec(schemaName, "icd9", "code"))))))), 
                 new ColumnSpec[]{new ColumnSpec(schemaName, "icd9", "hadm_id"), new ColumnSpec(schemaName, "icd9", "sequence")}, 
@@ -120,10 +120,10 @@ public class MIMIC2v26DataSourceBackend extends RelationalDbDataSourceBackend {
                 null, 
                 new PropertySpec[]{
                     new PropertySpec("code", null, new ColumnSpec(schemaName, "icd9", "code"), ValueType.NOMINALVALUE), 
-                    new PropertySpec("position", null, new ColumnSpec(schemaName, "icd9", "sequence", Operator.EQUAL_TO, this.mapper.propertyNameOrPropIdToSqlCodeArray("icd9_diagnosis_position_09102013.txt")), ValueType.NOMINALVALUE)}, 
+                    new PropertySpec("position", null, new ColumnSpec(schemaName, "icd9", "sequence", Operator.EQUAL_TO, getMappingsFactory().getInstance("icd9_diagnosis_position_09102013.txt")), ValueType.NOMINALVALUE)}, 
                 null, 
                 null, 
-                new ColumnSpec(schemaName, "icd9", "code", Operator.EQUAL_TO, this.mapper.propertyNameOrPropIdToSqlCodeArray("icd9_diagnosis_09102013.txt"), true), 
+                new ColumnSpec(schemaName, "icd9", "code", Operator.EQUAL_TO, icd9DxMappings, true), 
                 null, null, null, AbsoluteTimeGranularity.DAY, jdbcTimestampPositionParser, AbsoluteTimeUnit.YEAR)
         };
         return eventSpecs;
